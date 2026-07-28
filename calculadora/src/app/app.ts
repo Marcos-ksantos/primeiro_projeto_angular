39e7729def0645fe5f53416cdcc877b923cb00af
Marcos <marcos.k.santos@aluno.senai.br>
2026-07-28 14:54:01 -0300

    Calculadora Finalizada

--- calculadora/src/app/app.ts
+++ calculadora/src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CalculadoraComponent } from './calculadora/calculadora';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CalculadoraComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('calculadora');
-}
+export class AppComponent {}
